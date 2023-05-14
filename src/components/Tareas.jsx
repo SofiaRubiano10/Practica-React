24e7585be97cdb925dfad51b9c1d2172ea2d8397
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-05-14 14:44:06 +0200

    CSS inline

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -6,14 +6,26 @@ const Tareas = () => {
     {id:3, titulo: "Reading"},
     {id:4, titulo: "Swimming"},
   ];
+
+  // objeto JSON para representar el style
+  const estiloSpan = {
+    color: "lime",
+    fontStyle: "italic",
+  };
+
+
+
     return (
     <>
-        {listaTareas.length > 0 ? (<h2>Hay {listaTareas.length} tareas</h2>) : (<h2>No hay tareas</h2>)}
-        <ul>
-            {listaTareas.map(tarea => (
-            <li className="tarea" key={tarea.id}>{tarea.titulo}</li>
-            ))}
-        </ul>
+    {/* comentario */}
+      <h2 style={{color: "red", backgroundColor: "pink"}}>Lista de tareas</h2>
+      <span style={estiloSpan} >List of task</span>
+      {listaTareas.length > 0 ? (<h2>Hay {listaTareas.length} tareas</h2>) : (<h2>No hay tareas</h2>)}
+      <ul>
+          {listaTareas.map(tarea => (
+          <li className="tarea" key={tarea.id}>{tarea.titulo}</li>
+          ))}
+      </ul>
     </>
 
   );
