87cc389ac4bd06362a7fa246e639696f07deef1a
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-05-14 12:35:17 +0200

    Refacture

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 // importes
 import Header from "./components/Header"
-import Opcion from "./components/Opcion"
 // componente basado en función
 function App() {
   return (
   <>
     <Header />
-    <Opcion texto="Home" link="/"/>
-    <Opcion texto="Products" link="/products"/>
-    <Opcion texto="Services" link="/services"/>
-    <Opcion />
+
   </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import Opcion from "./Opcion"
 const Header = () => {
   return (
-    <header>Frontend Intermedio</header>
+    <header>
+      <h1>Frontend Intermedio</h1>
+      <Opcion texto="Home" link="/"/>
+      <Opcion texto="Products" link="/products"/>
+      <Opcion texto="Services" link="/services"/>
+      <Opcion />
+    </header>
   )
 }
 
