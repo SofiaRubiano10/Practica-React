36a5aae8a65c75b02b4489bfb9f2e773e21401b3
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-05-19 14:37:01 +0200

    Eliminación componente Boton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import PropTypes from "prop-types";
-import Boton from "./Boton";
+
 
 const Header = ({titulo}) => {
-  
-  const procesarClick = () =>{
-    console.log("Click en el componente padre");
-  }
   
   return (
     <header>
       <h1>{titulo}</h1>
-      <Boton cuandoClick={procesarClick}/>
     </header>
   )
 }
