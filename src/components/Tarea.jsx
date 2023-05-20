d4a302992466537c5fda802d25ee123ece4a9450
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-05-20 14:13:34 +0200

    Doble clik

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -2,8 +2,12 @@ import PropTypes from "prop-types";
 import {FaTrashAlt} from 'react-icons/fa';
 
 const Tarea = ({tarea, onDelete}) => {
+  const funcion = () => {
+    console.log("Doble Click");
+  }
+  
   return (
-    <li className={tarea.terminada ? "terminada" : ""}>
+    <li className={tarea.terminada ? "terminada" : ""} onDoubleClick={funcion}>
       {tarea.titulo} 
       <FaTrashAlt className="icono" onClick={() => onDelete(tarea.id)}/>
     </li>
